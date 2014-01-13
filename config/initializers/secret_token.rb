a279ea36917472030cba0cb8073096dfcc0da841
hiren mistry <hiren.mistry@chai-monsters.com>
2014-01-12 21:50:33 -0800

    Initial application commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ContactManager::Application.config.secret_key_base = 'ade4bd09512618e65d153953b027f546c0888ff72767a0e1f03fb602ed8d89964398e6477a0d75ebf88342d6116402eda45fc73eaa02c6bee0a153f7b3935eb4'
