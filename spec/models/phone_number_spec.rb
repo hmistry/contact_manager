ee043f02b9e0c444343e0801070db5905382900e
hiren mistry <hiren.mistry@chai-monsters.com>
2014-01-13 19:17:29 -0800

    Added validation and person association to phone_number model

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe PhoneNumber do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) {PhoneNumber.new(number: "1112223333", person_id: 1)}
+
+  it "is valid" do
+    expect(phone_number).to be_valid
+  end
+
+  it "is not valid without a number" do
+    phone_number.number = nil
+    expect(phone_number).not_to be_valid
+  end
+
+  it "must be referenced to a person" do
+    phone_number.person_id = nil
+    expect(phone_number).not_to be_valid
+  end
+
+  it "is associated with a person" do
+    expect(phone_number).to respond_to(:person)
+  end
 end
