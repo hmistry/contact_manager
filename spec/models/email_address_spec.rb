a446a81c25aace1346a796d5023e43bbb3d75d6a
hiren mistry <hiren.mistry@chai-monsters.com>
2014-01-23 12:31:38 -0800

    EmailAddress validation and association added including spec tests

--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe EmailAddress do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:email_address) { EmailAddress.new(address: "alice.smith@smith.com", person_id: 1) }
+
+  it 'is valid' do
+    expect(email_address).to be_valid
+  end
+
+  it 'is invalid without an address' do
+    email_address.address = nil
+    expect(email_address).to_not be_valid
+  end
+
+  it 'is associated with a person' do
+    expect(email_address).to respond_to(:person)
+  end
+
+  it 'must be associated with a person' do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
 end
