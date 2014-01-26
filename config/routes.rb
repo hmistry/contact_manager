7be4dcd9b500311d832045132b897a13660f8aff
hiren mistry <hiren.mistry@chai-monsters.com>
2014-01-26 02:20:07 -0800

    Removed index & show actions/routes from EmailAddress & PhoneNumber

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 ContactManager::Application.routes.draw do
   resources :companies
 
-  resources :email_addresses
+  resources :email_addresses, except: [:show, :index]
 
-  resources :phone_numbers
+  resources :phone_numbers, except: [:show, :index]
 
   resources :people
 
